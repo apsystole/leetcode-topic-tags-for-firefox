8b0cd6829545aace3ad70bebb2ca14876191cdf2
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2025-03-11 18:01:16 +0100

    unindent

--- content.js
+++ content.js
@@ -1,31 +1,33 @@
-(function () {
-    console.log("LeetCode Extension starting")
+console.log("LeetCode extension starting")
 
-    const catchButton = new MutationObserver(() => {
-        let button = document.querySelector('#headlessui-popover-button-\\:rf\\:')
-        if (button) {
-            catchButton.disconnect()
+const catchButton = new MutationObserver(() => {
+    let button = document.querySelector('#headlessui-popover-button-\\:rf\\:')
+    if (button) {
+        catchButton.disconnect()
+        setTimeout(() => {
+            button.click()
+            console.log("LeetCode extension click 1")
+
             setTimeout(() => {
-                button.click()
-                console.log("LeetCode Extension click 1")
+                button = document.querySelector('#headlessui-popover-button-\\:rf\\:')
+                // Since there is no easy stable selector (id/class), assume that:
+                // - assume the menu div is always button's immediate sibling,
+                // - assume it has another div inside,
+                // - assume "Show topic tags" is always the first item in that div.
+                button.nextSibling.childNodes[0].childNodes[0].click()
+                console.log("LeetCode extension click 2")
+
                 setTimeout(() => {
-                    button = document.querySelector('#headlessui-popover-button-\\:rf\\:')
-
-                    button.nextSibling.childNodes[0].childNodes[0].click()
-                    console.log("LeetCode Extension click 2")
-
-                    setTimeout(() => {
-                        // Now close the popup neatly.
-                        document.querySelector('#headlessui-popover-button-\\:rf\\:').click()
-                        console.log("LeetCode Extension click 3")
-                    }, 500)
+                    // Neatly close the popup menu.
+                    document.querySelector('#headlessui-popover-button-\\:rf\\:').click()
+                    console.log("LeetCode extension click 3")
                 }, 500)
-            }, 3000)
-        }
-    })
+            }, 500)
+        }, 3000)
+    }
+})
 
-    catchButton.observe(document.body, {
-        childList: true,
-        subtree: true
-    })
-})()
+catchButton.observe(document.body, {
+    childList: true,
+    subtree: true
+})
